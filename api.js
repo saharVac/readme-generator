41585e85d55082faeab6cf8d1188012368b859a7
saharVac <vcsahar@gmail.com>
2020-10-21 18:48:52 -0400

    Fixed functionality that checks whether git user exists or not

--- api.js
+++ api.js
@@ -1,10 +1,17 @@
 // CHECKING IF GITHUB USER EXISTS
+const axios = require("axios")
 
-const api = {
-  getUser(username) {
-    // check for an error
-    
+async function userExists(username) {
+  let accountExists;
+  const queryUrl = `https://api.github.com/users/${username}`
+  try {
+    const response = await axios.get(queryUrl)
+    .then( () => {accountExists = true})
+    .catch( () => {accountExists = false})
+  } catch (err) {
+    console.log(err);
   }
-};
+  return accountExists
+}
 
-module.supports = api;
+module.exports = userExists;
--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // import dependencies
 const fs = require("fs");
 const inquirer = require("inquirer");
+const userExists = require("./api.js");
 const api = require("./api.js")
 const generateMarkdown = require("./generateMarkdown.js")
 
+// variable that assures gitHub account exists
+var gitExists = false;
+let account;
+
+// Question to obtain git account
+const getGit = {
+  type: 'input',
+  name: 'github',
+  message: "what's your GitHub Account?"
+}
+
 // array of questions for user
 const questions = [
-  {
-    type: 'input',
-    name: 'github',
-    message: "what's your GitHun Username?"
-  },
   {
     type: 'input',
     name: 'title',
@@ -50,11 +57,32 @@ function writeToFile(fileName, data) {
 }
 
 // function to initialize program
-function init() {
-  inquirer.prompt(questions).then((response) => {
-    console.log(response);
+async function init() {
+  
+  // while no proper gitHub account name is given
+  while (!gitExists) {
+    
+    // if real account is given, gitHub account exists and we can move on
+    const {github: account} = await inquirer.prompt(getGit)
+    const k = await userExists(account)
+    console.log(k)
+    if (k) {
+      gitExists = true;
+      console.log("here")
+      console.log("username exists!")
+    } else {
+      console.log("username doesn't exist, try again")
+    }
+  }
 
-  });
+  // store response
+  const response = await inquirer.prompt(questions)
+  // write to file
+  fs.writeFile("README.md", generateMarkdown(response, account), function(err) {
+    if (err) {
+      return console.log(err)
+    }
+  })
 }
 
 // function call to initialize program
